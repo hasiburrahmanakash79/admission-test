2fe9069c77378f54994b09d450d8d98054182836
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-07-22 23:21:17 +0600

    make fake data

--- src/Pages/College/College.jsx
+++ src/Pages/College/College.jsx
@@ -1,10 +1,26 @@
-
 const College = () => {
-    return (
-        <div>
-            <h1>All collages</h1>
+  return (
+    <div>
+      <h1>All collages</h1>
+      <div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://plus.unsplash.com/premium_photo-1661930029003-0404b6e917f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29sbGVnZSUyMGNhbXB1c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default College;
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -19,39 +19,41 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className=" bg-base-100">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {NavItem}
-          </ul>
+              {NavItem}
+            </ul>
+          </div>
+          <h1 className="text-2xl font-extrabold">Admission Test</h1>
         </div>
-        <h1 className="text-2xl font-extrabold">Admission Test</h1>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{NavItem}</ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn btn-warning btn-sm">Apply Now</a>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{NavItem}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-warning btn-sm">Apply Now</a>
+        </div>
       </div>
     </div>
   );
