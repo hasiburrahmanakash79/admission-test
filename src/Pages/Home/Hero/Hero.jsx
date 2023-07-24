d9936c0cae61ab021a6a122b83fa52f6016d0c89
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-07-24 23:12:40 +0600

    make private routes

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,3 +1,5 @@
+import { FaSearch } from "react-icons/fa";
+
 const Hero = () => {
   return (
     <div>
@@ -12,15 +14,24 @@ const Hero = () => {
           <div className="max-w-xl py-14">
             <h1 className="mb-5 text-5xl font-bold">Hello Dear Student</h1>
             <p className="mb-5">
-            The college application process can be competitive, but with thorough preparation, a strong academic record, and a well-crafted application, you can increase your chances of admission. Best of luck on your journey to higher education.
+              The college application process can be competitive, but with
+              thorough preparation, a strong academic record, and a well-crafted
+              application, you can increase your chances of admission. Best of
+              luck on your journey to higher education.
             </p>
             <div className="flex justify-center items-center gap-2 my-3">
-              <input
-                type="text"
-                placeholder="Search College Name"
-                className="input input-bordered input-primary w-full max-w-xs"
-              />
-              <button className="btn btn-primary">Search</button>
+              <div className="form-control my-5">
+                <div className="input-group">
+                  <input
+                    type="text"
+                    placeholder="Search College"
+                    className="input input-bordered text-black"
+                  />
+                  <button className="btn btn-square">
+                    <FaSearch></FaSearch>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
