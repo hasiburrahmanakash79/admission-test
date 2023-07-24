f03a8c95ac32ab73b8548cad82ae8c5404a9bb52
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-07-24 13:48:24 +0600

    make a apply section

--- src/Pages/Apply/Apply.jsx
+++ src/Pages/Apply/Apply.jsx
@@ -1,133 +1,174 @@
-import { useContext } from 'react';
-import UseTitle from '../../Hook/UseTitle';
-import Swal from 'sweetalert2';
-import { useForm } from 'react-hook-form';
-import { Authentication } from '../../Provider/AuthProvider';
+import { useContext } from "react";
+import UseTitle from "../../Hook/UseTitle";
+import Swal from "sweetalert2";
+import { useForm } from "react-hook-form";
+import { Authentication } from "../../Provider/AuthProvider";
 
 const Apply = () => {
-    const { user } = useContext(Authentication);
- 
-//   const Image_Hosting_Token = import.meta.env.VITE_image_hosting_apiKey
+  const { user } = useContext(Authentication);
 
-  const { register, handleSubmit} = useForm();
+  const Image_Hosting_Token = import.meta.env.VITE_image_hosting_apiKey;
 
-//   const image_hosting_url = `https://api.imgbb.com/1/upload?key=${Image_Hosting_Token}`
+  const { register, handleSubmit } = useForm();
 
-  const onSubmit = data => {
+  const image_hosting_url = `https://api.imgbb.com/1/upload?key=${Image_Hosting_Token}`;
+
+  const onSubmit = (data) => {
     const formData = new FormData();
-    formData.append('image', data.image[0])
+    formData.append("image", data.image[0]);
 
-    // fetch(image_hosting_url, {
-    //   method: "POST",
-    //   body: formData
-    // })
-    // .then(res => res.json())
-    // .then(imageResponse => {
-    //   if(imageResponse.success){
-    //     const imgURL = imageResponse.data.display_url;
-    //     const {name, price, instructor_name, email, available_seat} = data;
-    //     const addClass = {name, price: parseInt(price), instructor_name, email, available_seat: parseInt(available_seat), image: imgURL, status: "pending"}
-        
-    //     .then(data => {
-    //       if(data.data.insertedId){
-    //         Swal.fire({
-    //           showConfirmButton: false,
-    //           timer: 1500,
-    //           title: "Item added Successful",
-    //           icon: "success",
-    //         });
-    //       }
-    //     })
-    //   }
-    // })
+    fetch(image_hosting_url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imageResponse) => {
+        if (imageResponse.success) {
+          const imgURL = imageResponse.data.display_url;
+          const { name, subject, number, email, date, address } = data;
+          const apply = {
+            name,
+            subject,
+            number,
+            email,
+            date,
+            image: imgURL,
+            address,
+            status: "Confirm",
+          };
+          console.log(data);
+          fetch("http://localhost:5000/apply", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json"
+            },
+            body: JSON.stringify(apply)
+          })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            // if(data.data.insertedId){
+              Swal.fire({
+                showConfirmButton: false,
+                timer: 1500,
+                title: "Item added Successful",
+                icon: "success",
+              });
+            
+          })
+        }
+      });
   };
 
-    return (
-        <div className="p-5">
-          <UseTitle title={"add classes"}></UseTitle>
-    
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="bg-base-200 p-10 rounded-lg w-full"
-          >
-            <div>
-              <label className="label">
-                <span className="label-text">Class name</span>
-              </label>
-              <input
-                type="text"
-                {...register("name", { required: true })}
-                placeholder="Type here"
-                className="input w-full"
-              />
-            </div>
-            <div className="md:grid grid-cols-2 gap-5">
-              <div>
-                <label className="label">
-                  <span className="label-text">Instructor Name</span>
-                </label>
-                <input
-                  {...register("instructor_name", { required: true })}
-                  type="text"
-                  defaultValue={user.displayName}
-                  placeholder="Type here"
-                  className="input w-full"
-                />
-              </div>
-              <div>
-                <label className="label">
-                  <span className="label-text">Instructor Email</span>
-                </label>
-                <input
-                  {...register("email", { required: true })}
-                  type="text"
-                  defaultValue={user.email}
-                  placeholder="Type here"
-                  className="input w-full"
-                />
-              </div>
-            </div>
-            <div className="grid grid-cols-2 gap-5">
-              <div>
-                <label className="label">
-                  <span className="label-text">Price</span>
-                </label>
-                <input
-                  {...register("price", { required: true })}
-                  type="number"
-                  placeholder="Type here"
-                  className="input w-full"
-                />
-              </div>
-              <div>
-                <label className="label">
-                  <span className="label-text">Available Seat</span>
-                </label>
-                <input
-                  {...register("available_seat", { required: true })}
-                  type="number"
-                  placeholder="Type here"
-                  className="input w-full"
-                />
-              </div>
-            </div>
-            <div>
-              <input
-                type="file"
-                {...register("image", { required: true })}
-                className="file-input w-full my-3"
-              />
-            </div>
-            <div>
-              <input
-                type="submit"
-                value="Add Class"
-                className="btn btn-outline order-none w-full my-5"
-              />
-            </div>
-          </form>
+  return (
+    <div className="p-5 container mx-auto">
+      <UseTitle title={"Apply college"}></UseTitle>
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-base-200 p-10 rounded-lg w-full"
+      >
+        <div>
+          <label className="label">
+            <span className="label-text">Candidate Name</span>
+          </label>
+          <input
+            type="text"
+            {...register("name", { required: true })}
+            placeholder="Type here"
+            className="input w-full"
+          />
         </div>
-      );
+        <div className="md:grid grid-cols-2 gap-5">
+          <div>
+            <label className="label">
+              <span className="label-text">Candidate Email</span>
+            </label>
+            <input
+              {...register("email", { required: true })}
+              type="text"
+              defaultValue={user?.email}
+              placeholder="Type here"
+              className="input w-full"
+            />
+          </div>
+          <div>
+            <label className="label">
+              <span className="label-text">Phone</span>
+            </label>
+            <input
+              {...register("number", { required: true })}
+              type="number"
+              defaultValue={user?.displayName}
+              placeholder="Type here"
+              className="input w-full"
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-5">
+          <div>
+            <label className="label">
+              <span className="label-text">Subject</span>
+            </label>
+            <select
+              className="select w-full"
+              {...register("subject", { required: true })}
+            >
+              <option disabled >
+                Subject select
+              </option>
+              <option >Science</option>
+              <option>Mathematics</option>
+              <option>Chemistry</option>
+              <option>Physics</option>
+              <option>Computer Science</option>
+              <option>EEE</option>
+              <option>LLB</option>
+              <option>Genetics engineering</option>
+              <option>BBA</option>
+              <option>English</option>
+            </select>
+          </div>
+          <div>
+            <label className="label">
+              <span className="label-text">Date of Birth</span>
+            </label>
+            <input
+              {...register("date", { required: true })}
+              type="date"
+              placeholder="Type here"
+              className="input w-full"
+            />
+          </div>
+        </div>
+        <div>
+          <label className="label">
+            <span className="label-text">Address</span>
+          </label>
+          <textarea
+            type="text"
+            {...register("address", { required: true })}
+            placeholder="Address"
+            className="input w-full py-2"
+          />
+        </div>
+        <div>
+          <input
+            type="file"
+            {...register("image", { required: true })}
+            className="file-input w-full my-3"
+          />
+        </div>
+        <div>
+          <input
+            type="submit"
+            value="Submit"
+            className="btn btn-outline order-none w-full my-5"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Apply;
